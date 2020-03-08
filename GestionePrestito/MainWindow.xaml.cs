1172775e3869ba6cec6d1aebcfa5a5ad44bfe2de
Alessio Manieri <mani.alexander03@gmail.com>
2020-03-08 16:55:34 +0100

    finito

--- GestionePrestito/MainWindow.xaml.cs
+++ GestionePrestito/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,24 +25,36 @@ namespace GestionePrestito
         {
             InitializeComponent();
         }
-
+        string combo="";
         private void Calcola_Click(object sender, RoutedEventArgs e)
         {
-            double nrate = double.Parse(txtrate.Text);
-            double importo = double.Parse(txtimporto.Text);
-            double tasso = double.Parse(txttasso.Text);
-            double impcontasso=importo/100*tasso ;
-            double rate = impcontasso / nrate;
-            string combo = combocitta.Text;
-            if (femmina.IsChecked == true)
+            try
             {
-                lblresult.Content += $"{txtcognome.Text} {txtnome.Text}, residente in {combo} nata il {data.SelectedDate.Value.ToShortDateString()}. Prestito di {txtimporto.Text} ad un tasso del {txttasso.Text} da restituire in {txtrate.Text} rate da {rate} euro ciascuna, per un totale di {impcontasso}";
+                double nrate = double.Parse(txtrate.Text);
+                double importo = double.Parse(txtimporto.Text);
+                double tassoperc = double.Parse(txttasso.Text);
+                double tasso = importo / 100 * tassoperc;
+                double tot = tasso + importo;
+                double rate = tot / nrate;
+                combo = combocitta.Text;
+                if (femmina.IsChecked == true)
+                {
+                    lblresult.Content += $"{txtcognome.Text} {txtnome.Text}, residente in {combo} nata il {data.SelectedDate.Value.ToShortDateString()}. Prestito di {txtimporto.Text} ad un tasso del {txttasso.Text} da restituire in {txtrate.Text} rate da {rate} euro ciascuna, per un totale di {tot}\n";
+                }
+                if (maschio.IsChecked == true)
+                {
+                    lblresult.Content += $"{txtcognome.Text} {txtnome.Text}, residente in {combo} nato il {data.SelectedDate.Value.ToShortDateString()}. Prestito di {txtimporto.Text} ad un tasso del {txttasso.Text} da restituire in {txtrate.Text} rate da {rate} euro ciascuna, per un totale di {tot}\n";
+                }
+                btnnuovo.IsEnabled = true;
+                txtrestituire.Text=tot.ToString();
+                txtimpxrata.Text = rate.ToString();
+
             }
-            if (maschio.IsChecked == true)
+            catch
             {
-                lblresult.Content+= $"{txtcognome.Text} {txtnome.Text}, residente in {combo} nato il {data.SelectedDate.Value.ToShortDateString()}. Prestito di {txtimporto.Text} ad un tasso del {txttasso.Text} da restituire in {txtrate.Text} rate da {rate} euro ciascuna, per un totale di {impcontasso}";
+                throw new Exception("Inserire tutti i dati necessari");
             }
-            btnnuovo.IsEnabled = true;
+           
         }
 
         private void Nuovo_Click_1(object sender, RoutedEventArgs e)
@@ -52,10 +65,31 @@ namespace GestionePrestito
             txtrate.Text = null;
             txtrestituire.Text = null;
             txttasso.Text = null;
-            femmina.IsChecked = false ;
+            txtimpxrata.Text = null;
+            femmina.IsChecked = false;
             maschio.IsChecked = false;
             data.SelectedDate = null;
             combocitta.SelectedItem = null;
+        }
+
+        private void Btnstampa_Click(object sender, RoutedEventArgs e)
+        {
+
+            using (StreamWriter sw = new StreamWriter("file.csv", false, Encoding.UTF8))
+            {
+                sw.WriteLine("Cognome;Nome;Città;Sesso;Data di nascita;Importo richiesto (€);% di interesse; Numero di rate;Importo Rata (€);Totale € da restituire");
+                if (femmina.IsChecked == true)
+                {
+                    sw.WriteLine($"{txtcognome.Text}; {txtnome.Text};{combo};F;{data.SelectedDate.Value.ToShortDateString()};{txtimporto.Text};{txttasso.Text};{txtrate.Text};{txtimpxrata.Text};{txtrestituire.Text}");
+                }
+                else
+                {
+                    sw.WriteLine($"{txtcognome.Text}; {txtnome.Text};{combo};M;{data.SelectedDate.Value.ToShortDateString()};{txtimporto.Text};{txttasso.Text};{txtrate.Text};{txtimpxrata.Text};{txtrestituire.Text}");
+                }
+                //Funzionante, fatto senza lista
+            
+            }
+            
         }
     }
 }
